40276069783e14c087d9c194731ff81b63550f39
admjob <admjob@163.com>
2020-07-11 14:56:00 +0800

    hello word

---
+++ src/main/java/com/tom/test1/com/tom/test1/helloword01.java
@@ -0,0 +1,11 @@
+package com.tom.test1.com.tom.test1;
+
+/**
+ * @author Tom
+ * @create 2020-07-11 14:36
+ */
+public class helloword01 {
+    public static void main(String[] args) {
+        System.out.println("hello word!!!");
+    }
+}
---
+++ src/main/java/com/tom/test1/helloword.java
@@ -0,0 +1,13 @@
+package com.tom.test1;
+
+/**
+ * @author Tom
+ * @create 2020-07-11 14:15
+ */
+public class helloword {
+    public static void main(String[] args) {
+        System.out.println("heloo word!!!");
+        System.out.println("heloo word!!!");
+        System.out.println("heloo word!!!");
+    }
+}
